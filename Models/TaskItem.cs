0204fd38884715f1186d5d635610f82cf55631ac
bryan <bryandev12@gmail.com>
2024-03-12 09:16:20 -0400

    Add autoincrement Id taskItem

--- Models/TaskItem.cs
+++ Models/TaskItem.cs
@@ -1,7 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace TaskListApi.Models
 {
     public class TaskItem
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Title { get; set; }
         public bool IsCompleted { get; set; }
