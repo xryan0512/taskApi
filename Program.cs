928c92138169a121d356cdaba22b4e6c1798288f
bryan <bryandev12@gmail.com>
2024-03-12 09:18:16 -0400

    Add cors Allow specific origin

--- Program.cs
+++ Program.cs
@@ -9,7 +9,17 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<TaskListContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("TaskListDatabase")));
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+    builder => builder.WithOrigins("http://localhost:4200")
+    .AllowAnyHeader()
+    .AllowAnyMethod());
+});
+
 var app = builder.Build();
+
+app.UseCors("AllowSpecificOrigin"); // Use the CORS policy
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
